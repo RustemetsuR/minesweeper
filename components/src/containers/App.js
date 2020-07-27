efc4db736b30e56020485d7512dc1882298c7629
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-07-27 15:14:22 +0600

    Added pushToArr() and makeArr() methods to push all objects to the cubes state

--- components/src/containers/App.js
+++ components/src/containers/App.js
@@ -3,6 +3,26 @@ import './App.css';
 
 const App = () => {
   const [cubes, setCubes] = useState([]);
+
+  const pushToArr = (cubesCopy) => {
+    for (let i = 0; i < 36; i++) {
+      cubesCopy.push({
+        id: 'id-' + i,
+        status: false,
+      })
+    }
+    setCubes(cubesCopy);
+  } 
+  
+const makeArr = () =>{
+  const cubesCopy = [...cubes];
+  if(cubesCopy.length === 36){
+    cubesCopy.length = 0;
+    pushToArr(cubesCopy);
+  }else{
+    pushToArr(cubesCopy);
+  }
+}
   return (
     <div className="App">
      
